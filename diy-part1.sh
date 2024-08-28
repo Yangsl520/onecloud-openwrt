ed32f430669b7c6ff78358f3122abfb6a6163005
Yangsl520 <47134834+Yangsl520@users.noreply.github.com>
2024-08-29 02:12:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,8 @@
 # Add a feed source
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> "feeds.conf.default"
+echo "src-git small https://github.com/kenzok8/small" >> "feeds.conf.default"
 
 git clone https://github.com/sbwml/luci-app-alist package/alist
 git clone https://github.com/rufengsuixing/luci-app-onliner package/onliner
