0c33ce40499b99f74e4d3272bce62df3be037fb0
Yangsl520 <47134834+Yangsl520@users.noreply.github.com>
2024-08-29 23:30:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,11 +14,11 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-# echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-# echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
-echo "src-git smpackage https://github.com/kenzok8/small-package" >> "feeds.conf.default"
-# echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> "feeds.conf.default"
-# echo "src-git small https://github.com/kenzok8/small" >> "feeds.conf.default"
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+# echo "src-git smpackage https://github.com/kenzok8/small-package" >> "feeds.conf.default"
+echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> "feeds.conf.default"
+echo "src-git small https://github.com/kenzok8/small" >> "feeds.conf.default"
 
 # git clone https://github.com/sbwml/luci-app-alist package/alist
-# git clone https://github.com/rufengsuixing/luci-app-onliner package/onliner
+git clone https://github.com/rufengsuixing/luci-app-onliner package/onliner
